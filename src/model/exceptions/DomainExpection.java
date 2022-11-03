0af27dc1c7560535405eab6413ba9740e0b194d4
Sinnisterr <willianbruno@msn.com>
2022-11-02 22:45:24 -0300

    Versao boa

---
+++ src/model/exceptions/DomainExpection.java
@@ -0,0 +1,11 @@
+package model.exceptions;
+
+public class DomainExpection extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	
+	public DomainExpection( String msg ) {
+		super(msg);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author willi
+ *
+ */
+module exceptions2.java {
+}
